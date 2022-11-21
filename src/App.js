a9c0acea6e1c6530920a982af164f797825986a0
Dan-Sutton <dewasutton@gmail.com>
2022-11-21 12:23:41 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Card />
       </header>
+      <footer className="app-footer">
+        <div className="bottom-nav">
+          <p>Created by Dan Sutton 2022</p>
+          <p>How to use</p>
+        </div>
+      </footer>
     </div>
   );
 }
