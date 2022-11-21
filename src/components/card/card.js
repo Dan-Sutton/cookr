f7e0ee4f79065cc3b13b9ea199534503df7454dc
Dan-Sutton <dewasutton@gmail.com>
2022-11-21 22:29:44 +0000

    card flips

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./card.css";
 
 function Card(props) {
-  return <div className="card"></div>;
+  return (
+    <div className="card">
+      <div className="card-front">Front</div>
+      <div className="card-back">Back</div>
+    </div>
+  );
 }
 
 export default Card;
