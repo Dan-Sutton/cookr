eb6fa856c2240e7313dd18fde6620bb0ae321bf2
Dan-Sutton <dewasutton@gmail.com>
2022-11-22 15:29:25 +0000

    fixed alignment

--- src/components/card/front-card.js
+++ src/components/card/front-card.js
@@ -9,10 +9,13 @@ function FrontCard({}) {
         alt="a tasty meal"
         src="https://www.seriouseats.com/thmb/QChcw6d_9s7rS4h57ADKBWEvxv4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-best-slow-cooked-bolognese-sauce-recipe-hero-03_1-3bf4f3401fa84c828f68071df496ddd3.JPG"
       />
-
-      <h2>Spaghetti with Meat Sauce</h2>
-      <p>🕐 45 mins</p>
-      <p>🥘 4 servings</p>
+      <div className="front-card-content">
+        <h2>Spaghetti with Meat Sauce</h2>
+        <div className="front-card-content-body">
+          <p>🕐 45 mins</p>
+          <p>🥘 4 servings</p>
+        </div>
+      </div>
     </div>
   );
 }
