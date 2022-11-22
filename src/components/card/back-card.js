16542eb6272f1b01da14701f50212287a57b1b06
Dan-Sutton <dewasutton@gmail.com>
2022-11-22 20:24:29 +0000

    back of card

--- src/components/card/back-card.js
+++ src/components/card/back-card.js
@@ -4,10 +4,20 @@ function BackCard({ recipe }) {
   return (
     <div className="front-card-component">
       <h1>Ingredients</h1>
-      <div className="back-card-content">
-        <div className="back-card-content-body">
+      {recipe.extendedIngredients != null ? (
+        <ul className="recipe-list">
+          {recipe.extendedIngredients.map((i) => {
+            return <li>{i.original}</li>;
+          })}
+        </ul>
+      ) : (
+        <div>
           <p>Ingredients will be listed here.</p>
         </div>
+      )}
+
+      <div className="back-card-content">
+        <div className="back-card-content-body"></div>
       </div>
     </div>
   );
